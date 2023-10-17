403a1775c6130b903fff9a60ae7bb2cf49f3ba91
Fitriyanii <sitifitriani8910@gmail.com>
2023-10-17 20:47:46 +0700

    Quiz

---
+++ main.js
@@ -0,0 +1,8 @@
+import { tambah } from "./mathOperation.js";
+console.log("Hasil Penjumlahan : " + tambah(9,8));
+
+import { kali } from "./mathOperation.js";
+console.log("Hasil Perkalian : " + kali(8,7));
+
+import { phi } from "./mathOperation.js";
+console.log("Nilai Pi : " + phi(3.14159));
---
+++ mathOperation.js
@@ -0,0 +1,9 @@
+export function tambah(a,b){
+    return a+b;
+}                                                                                                            // ECMAscript
+export function kali(a,b){
+    return a*b;
+}                                                                                                            // ECMAscript
+export function phi(a){
+    return a;
+}                                                                                                            // ECMAscript
